51097f9a300fe03b27a11ac695aa5c97b23fb54a
UwU-tao <hieungo1410@gmail.com>
2024-04-21 00:58:45 +0700

    add

--- src/dataset.py
+++ src/dataset.py
@@ -21,7 +21,7 @@ class ImgDataset(Dataset):
         img_path = self.images[idx]
         
         img = Image.open(img_path).convert('RGB')
-        target = [] * len(self.targets)
+        target = [] * 252
         target[self.targets[idx]] = 1
         
         if self.transform:
