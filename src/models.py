4a7627406826f0e05b8d5bd952fc422e35b53e1e
UwU-tao <hieungo1410@gmail.com>
2024-04-21 09:44:18 +0700

    add

--- src/models.py
+++ src/models.py
@@ -6,9 +6,9 @@ class CNN(nn.Module):
     def __init__(self, num_classes):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.pool = nn.MaxPool2d(4, 4)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(32 * 32, 120)
+        self.fc1 = nn.Linear(32 * 32 * 32, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_classes)
 
