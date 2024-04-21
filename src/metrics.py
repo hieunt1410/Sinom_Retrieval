1775c4bc540e3ca0ea08394df651fff0b697b468
UwU-tao <hieungo1410@gmail.com>
2024-04-21 10:08:40 +0700

    add

--- src/metrics.py
+++ src/metrics.py
@@ -8,5 +8,5 @@ mrr.to(device)
 
 def metrics(results, truths):
     # results, indices = torch.topk(results, 5)
-    indexes = torch.ones((truths.shape[0], len(truths)))
+    indexes = torch.ones((truths.shape[0], truths.shape[1]), dtype=torch.float32)
     return mrr(torch.tensor(results), torch.tensor(truths), indexes=indexes)
