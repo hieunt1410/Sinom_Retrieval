d758d463e0a6c1c55ec7e9525f67f0685c74b248
UwU-tao <hieungo1410@gmail.com>
2024-04-23 17:53:59 +0700

    add

---
+++ src/infer.py
@@ -0,0 +1,29 @@
+import torch
+import numpy as np
+import pandas as pd
+from PIL import Image
+from torchvision import transforms
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--img_path', type=str, default='data/queries/228.png')
+parser.add_argument('--model_path', type=str, default='pretrained_models/default_model.pt')
+args = parser.parse_args()
+
+transform=transforms.Compose([
+    transforms.Resize((512, 512)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+def infer(img_path, model_path='pretrained_models/default_model.pt'):
+    torch.load(model_path)
+    img = Image.open(img_path).convert('RGB')
+    img = transform(img)
+    output = model(img)
+    _, cols = torch.topk(output, 5)
+    return cols
+    
+
+if __name__ == '__main__':
+    print(infer(args.img_path, args.model_path))
--- src/metrics.py
+++ src/metrics.py
@@ -9,6 +9,6 @@ mrr.to(device)
 
 def metrics(results, truths):
     # all_reciprocal_ranks = [retrieval_reciprocal_rank(results[i], truths[i]) for i in range(len(truths))]
-    indexes = (torch.zeros_like(results[0]) + torch.arange(results.shape[0], device=device, dtype=torch.long).view(-1, 1)).flatten()
+    indexes = (torch.zeros_like(results[0]) + torch.arange(results.shape[0], device=device, dtype=torch.LongInt).view(-1, 1)).flatten()
     # return torch.mean(torch.stack(all_reciprocal_ranks))
     return mrr(results.flatten(), truths.flatten(), indexes)
