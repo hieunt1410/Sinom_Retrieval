9a9d5798df4d5ac26c5c6b5f9d333afdeba14f9a
UwU-tao <hieungo1410@gmail.com>
2024-04-23 17:27:22 +0700

    add

--- src/metrics.py
+++ src/metrics.py
@@ -1,12 +1,11 @@
 import torch
 import numpy as np
-from torchmetrics.retrieval import RetrievalMRR
+from torchmetrics.functional.retrieval import retrieval_reciprocal_rank
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-mrr = RetrievalMRR(top_k=5)
-mrr.to(device)
+# mrr = RetrievalMRR(top_k=5)
+# mrr.to(device)
 
 def metrics(results, truths):
-    # results, indices = torch.topk(results, 5)
-    indexes = torch.ones((truths.shape[0], truths.shape[1]), dtype=torch.long, device=device)
-    return mrr(torch.tensor(results), torch.tensor(truths), indexes=indexes)
+    all_reciprocal_ranks = [retrieval_reciprocal_rank(results[i], truths[i]) for i in range(len(truths))]
+    return torch.mean(torch.stack(all_reciprocal_ranks))
