19b94777d79db8ddd271899ad880b9a548fcb7b6
UwU-tao <hieungo1410@gmail.com>
2024-05-19 11:06:18 +0700

    add

--- infer.py
+++ infer.py
@@ -55,7 +55,7 @@ def compute_similar_features(image_path, num_images, embedding, device, nfeature
     keypoint_features, des = orb.compute(image, keypoint_features)
     
     des = des / 255.
-    des = des.expand_dims(des, axis=0)
+    des = np.expand_dims(des, axis=0)
     des = np.reshape(des, (des.shape[0], -1))
     
     pca = PCA(n_components=embedding.shape[1])
